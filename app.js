a38c143cbbe7617e7dd5aa5026be2a2bbc35425c
Katrien S <ontwerp@katriens.be>
2024-02-27 15:49:23 +0100

    JS Form Validation

--- app.js
+++ app.js
@@ -1,14 +1,75 @@
-const inputElements = document.querySelectorAll('input');
-const invalidMessage = document.querySelector('.invalid-message');
-const invalidSymbol = document.querySelector('.icon-tabler');
+const validateForm = (formSelector) => {
+	const formElement = document.querySelector(formSelector);
 
-console.log(invalidMessage);
+	const validationOptions = [
+		{
+			attribute: 'required',
+			isValid: (input) => input.value.trim() !== '',
+			errorMessage: (input, label) => `${label.textContent} is required`
+		},
+		{
+			attribute: 'minlength',
+			isValid: (input) =>
+				input.value && input.value.length >= parseInt(input.minLength, 10),
+			errorMessage: (input, label) =>
+				`${label.textContent} needs to be at least ${input.minLength} characters`
+		},
+		{
+			attribute: 'pattern',
+			isValid: (input) => {
+				const patternRegex = new RegExp(input.pattern);
+				return patternRegex.test(input.value);
+			},
+			errorMessage: (input, label) => `Not a valid ${label.textContent}`
+		}
+	];
 
-function validateInput() {
-	for (const inputElement of inputElements) {
-		const isValid = inputElement.checkValidity();
-		inputElement.setAttribute('aria-invalid', !isValid);
-		invalidMessage.style.visibility = !isValid ? 'visible' : 'hidden';
-		invalidSymbol.style.visibility = !isValid ? 'visible' : 'hidden';
-	}
-}
+	const validateSingleFormGroup = (formGroup) => {
+		const label = formGroup.querySelector('label');
+		const input = formGroup.querySelector('input, textarea');
+		const errorMessage = formGroup.querySelector('.error-message');
+		const errorIcon = formGroup.querySelector('.error-icon');
+
+		let formGroupError = false;
+		for (const option of validationOptions) {
+			if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
+				errorMessage.textContent = option.errorMessage(input, label);
+				input.style.borderColor = 'red';
+				errorIcon.classList.remove('hidden');
+
+				formGroupError = true;
+			}
+		}
+
+		if (!formGroupError) {
+			errorMessage.textContent = '';
+			input.style.borderColor = '#4ee1a0';
+			errorIcon.classList.add('hidden');
+		}
+	};
+
+	const validateAllFormGroups = (formToValidate) => {
+		const formGroups = Array.from(
+			formToValidate.querySelectorAll('.formgroup')
+		);
+
+		formGroups.forEach((formGroup) => {
+			validateSingleFormGroup(formGroup);
+		});
+	};
+
+	formElement.setAttribute('novalidate', '');
+
+	Array.from(formElement.elements).forEach((element) => {
+		element.addEventListener('blur', (event) => {
+			validateSingleFormGroup(event.srcElement.parentElement.parentElement);
+		});
+	});
+
+	formElement.addEventListener('submit', (event) => {
+		event.preventDefault();
+		validateAllFormGroups(formElement);
+	});
+};
+
+validateForm('#registrationForm');
